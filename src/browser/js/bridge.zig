c171cdd1f82de49fb109911eaae03b05853d9c73
adybag14-cyber <adybag14@gmail.com>
2026-03-14 18:47:37 +0000

    feat: deepen headed websocket indexeddb and webgl

--- src/browser/js/bridge.zig
+++ src/browser/js/bridge.zig
@@ -1118,6 +1118,7 @@ pub const PageJsApis = flattenTypes(&.{
     @import("../webapi/encoding/TextEncoderStream.zig"),
     @import("../webapi/encoding/TextDecoderStream.zig"),
     @import("../webapi/Event.zig"),
+    @import("../webapi/event/CloseEvent.zig"),
     @import("../webapi/event/CompositionEvent.zig"),
     @import("../webapi/event/CustomEvent.zig"),
     @import("../webapi/event/ErrorEvent.zig"),
