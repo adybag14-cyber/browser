7006cf10e690b8f7a52cb7bc9dbb71f2961f7ec2
adybag14-cyber <adybag14@gmail.com>
2026-05-18 00:09:54 +0100

    probes: force headed mode in shared smoke launchers

--- tmp-browser-smoke/file-upload/FileUploadProbeCommon.ps1
+++ tmp-browser-smoke/file-upload/FileUploadProbeCommon.ps1
@@ -89,7 +89,7 @@ function Wait-FileUploadServer([int]$Port, [int]$Attempts = 30) {
 }
 
 function Start-FileUploadBrowser([string]$StartupUrl, [string]$Stdout, [string]$Stderr) {
-  return Start-Process -FilePath $script:BrowserExe -ArgumentList 'browse',$StartupUrl,'--window_width','960','--window_height','640' -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
+  return Start-Process -FilePath $script:BrowserExe -ArgumentList @('browse','--browser_mode','headed','--window_width','960','--window_height','640',$StartupUrl) -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
 }
 
 function Wait-UploadDialogWindow([int]$ProcessId, [int]$Attempts = 40, [int]$SleepMs = 200) {
