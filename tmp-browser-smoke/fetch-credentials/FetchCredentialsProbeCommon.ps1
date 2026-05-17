2c12d4f15f104d45a77f0f7b327b888b38c7d086
adybag14-cyber <adybag14@gmail.com>
2026-05-18 00:10:46 +0100

    probes: force headed mode in shared smoke launchers

--- tmp-browser-smoke/fetch-credentials/FetchCredentialsProbeCommon.ps1
+++ tmp-browser-smoke/fetch-credentials/FetchCredentialsProbeCommon.ps1
@@ -41,7 +41,7 @@ function Wait-FetchServer([int]$Port, [int]$Attempts = 30) {
 }
 
 function Start-FetchBrowser([string]$StartupUrl, [string]$Stdout, [string]$Stderr) {
-  return Start-Process -FilePath $script:BrowserExe -ArgumentList "browse",$StartupUrl,"--window_width","960","--window_height","640" -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
+  return Start-Process -FilePath $script:BrowserExe -ArgumentList @("browse","--browser_mode","headed","--window_width","960","--window_height","640",$StartupUrl) -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
 }
 
 function Format-FetchProbeProcessMeta($Meta) {
