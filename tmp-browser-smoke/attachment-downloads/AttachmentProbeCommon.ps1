4352ec97c10ab295748996983533e70d68473a4c
adybag14-cyber <adybag14@gmail.com>
2026-05-18 00:10:52 +0100

    probes: force headed mode in shared smoke launchers

--- tmp-browser-smoke/attachment-downloads/AttachmentProbeCommon.ps1
+++ tmp-browser-smoke/attachment-downloads/AttachmentProbeCommon.ps1
@@ -41,7 +41,7 @@ function Start-AttachmentServer([int]$Port, [string]$Stdout, [string]$Stderr) {
 }
 
 function Start-AttachmentBrowser([string]$StartupUrl, [string]$Stdout, [string]$Stderr) {
-  return Start-Process -FilePath $script:BrowserExe -ArgumentList "browse",$StartupUrl,"--window_width","960","--window_height","640" -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
+  return Start-Process -FilePath $script:BrowserExe -ArgumentList @("browse","--browser_mode","headed","--window_width","960","--window_height","640",$StartupUrl) -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
 }
 
 function Invoke-AttachmentAddressCommit([IntPtr]$Hwnd, [string]$Url) {
