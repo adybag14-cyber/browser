cf24a2a78d704704ace001f6ec1dc2fa884612bc
adybag14-cyber <adybag14@gmail.com>
2026-05-18 00:09:47 +0100

    probes: force headed mode in shared smoke launchers

--- tmp-browser-smoke/cookie-persistence/CookieProbeCommon.ps1
+++ tmp-browser-smoke/cookie-persistence/CookieProbeCommon.ps1
@@ -46,7 +46,7 @@ function Start-CookieServer([int]$Port, [string]$Stdout, [string]$Stderr) {
 }
 
 function Start-CookieBrowser([string]$StartupUrl, [string]$Stdout, [string]$Stderr) {
-  return Start-Process -FilePath $script:BrowserExe -ArgumentList "browse",$StartupUrl,"--window_width","960","--window_height","640" -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
+  return Start-Process -FilePath $script:BrowserExe -ArgumentList @("browse","--browser_mode","headed","--window_width","960","--window_height","640",$StartupUrl) -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
 }
 
 function Invoke-CookieAddressCommit([IntPtr]$Hwnd, [string]$Url) {
