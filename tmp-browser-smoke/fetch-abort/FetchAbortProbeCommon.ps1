eff9719d8362e22fc181182dc659ca57614e8c7d
adybag14-cyber <adybag14@gmail.com>
2026-05-18 00:10:00 +0100

    probes: force headed mode in shared smoke launchers

--- tmp-browser-smoke/fetch-abort/FetchAbortProbeCommon.ps1
+++ tmp-browser-smoke/fetch-abort/FetchAbortProbeCommon.ps1
@@ -44,7 +44,7 @@ function Start-FetchAbortServer([int]$Port, [string]$Stdout, [string]$Stderr) {
 }
 
 function Start-FetchAbortBrowser([string]$StartupUrl, [string]$Stdout, [string]$Stderr) {
-  return Start-Process -FilePath $script:BrowserExe -ArgumentList "browse",$StartupUrl,"--window_width","960","--window_height","640" -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
+  return Start-Process -FilePath $script:BrowserExe -ArgumentList @("browse","--browser_mode","headed","--window_width","960","--window_height","640",$StartupUrl) -WorkingDirectory $script:Repo -PassThru -RedirectStandardOutput $Stdout -RedirectStandardError $Stderr
 }
 
 function Read-FetchAbortServerLog([string]$Path) {
